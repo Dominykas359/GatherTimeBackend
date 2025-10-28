0e4adfcc610d8ce2142ca480afd90caf352ce827
Dominykas Stunžėnas <dominykas.stunzenas@gmail.com>
2025-10-28 19:46:54 +0200

    initial commit

---
+++ gathertime/gathertime/src/main/java/com/app/gathertime/GathertimeApplication.java
@@ -0,0 +1,13 @@
+package com.app.gathertime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GathertimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GathertimeApplication.class, args);
+	}
+
+}
---
+++ gathertime/gathertime/src/test/java/com/app/gathertime/GathertimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.gathertime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GathertimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
